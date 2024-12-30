309615f521075c6b2909b3ff366974e75efffa91
bojack <1412393182@qq.com>
2024-12-30 21:49:35 +0800

    use mudblazor

--- src/blazor-wasm/Program.cs
+++ src/blazor-wasm/Program.cs
@@ -1,11 +1,14 @@
+using blazor_wasm;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using blazor_wasm;
+
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
